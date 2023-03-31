63116b116c22f1f1b9f09b6187ce89ea683781bb
aliklay <117722678+aliklay@users.noreply.github.com>
2023-03-31 23:35:35 +0200

    Create train_test.py

---
+++ ai_bot/train_test.py
@@ -0,0 +1,95 @@
+import numpy as np
+from keras.models import load_model
+from trading_environment import TradingEnvironment
+from dqn_model import DQNAgent
+from preprocess_data import preprocess_data
+from fetch_data import get_historical_data
+from elliott_wave_analysis import elliott_wave_analysis
+from binance.client import Client
+from sklearn.preprocessing import MinMaxScaler
+
+symbol = "BTCUSDT"
+interval = Client.KLINE_INTERVAL_15MINUTE
+start_str = "1 year ago UTC"
+end_str = "now"
+
+price_data_df = get_historical_data(symbol, interval, start_str, end_str)
+price_data = price_data_df['close'].astype(float).to_numpy()
+waves, corrections = elliott_wave_analysis(price_data)
+features = preprocess_data(price_data, waves, corrections)
+
+# Normalize the input features
+scaler = MinMaxScaler()
+features = scaler.fit_transform(features)
+
+train_data = features[:int(0.7 * len(features))]
+val_data = features[int(0.7 * len(features)):int(0.9 * len(features))]
+test_data = features[int(0.9 * len(features)):]
+
+env = TradingEnvironment(train_data)
+input_shape = env.observation_space.shape
+n_actions = env.action_space.n
+model = DQNAgent.create_dqn_model(input_shape, n_actions, timesteps=1)
+agent = DQNAgent(model, n_actions, epsilon_decay=0.995, gamma=0.99, batch_size=64, update_freq=1000, buffer_size=10000)
+
+n_episodes = 20
+best_val_reward = -np.inf
+model_weights_filepath = 'dqn_model_weights.h5'
+
+for episode in range(n_episodes):
+    state = env.reset()
+    state = np.reshape(state, (1, *state.shape))
+    done = False
+    episode_reward = 0
+
+    while not done:
+        action = agent.act(state)
+        next_state, reward, done, _ = env.step(action)
+        next_state = np.reshape(next_state, (1, *next_state.shape))
+
+        agent.train(state, action, reward, next_state, done)
+        episode_reward += reward
+        state = next_state
+
+    print(f"Episode {episode + 1}/{n_episodes} completed with reward {episode_reward}")
+
+    val_env = TradingEnvironment(val_data)
+    state = val_env.reset()
+    state = np.reshape(state, (1, *state.shape))
+    done = False
+    val_reward = 0
+
+    while not done:
+        action = agent.act(state)
+        next_state, reward, done, _ = val_env.step(action)
+        next_state = np.reshape(next_state, (1, *next_state.shape))
+
+        val_reward += reward
+        state = next_state
+
+    print(f"Validation reward: {val_reward}")
+
+    if val_reward > best_val_reward:
+        best_val_reward = val_reward
+        print(f"Saving model weights with validation reward {val_reward}")
+        model.save_weights(model_weights_filepath)
+
+best_model = DQNAgent.create_dqn_model(input_shape, n_actions)
+best_model.load_weights(model_weights_filepath)
+best_agent = DQNAgent(best_model, n_actions)
+
+test_env = TradingEnvironment(test_data)
+state = test_env.reset()
+state = np.reshape(state, (1, *state.shape))
+done = False
+total_reward = 0
+
+while not done:
+    action = best_agent.act(state)
+    next_state, reward, done, _ = test_env.step(action)
+    next_state = np.reshape(next_state, (1, *next_state.shape))
+
+    total_reward += reward
+    state = next_state
+
+print(f"Total reward on test data: {total_reward}")
